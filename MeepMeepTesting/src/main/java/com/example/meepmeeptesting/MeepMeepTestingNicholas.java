f18ae0fed114423eea24582bd42f68b04c0b8c40
loarado <brauliokoalas@gmail.com>
2024-10-07 16:56:09 -0500

    Nicholas path update

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingNicholas.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingNicholas.java
@@ -14,24 +14,12 @@ public class MeepMeepTestingNicholas {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(36, 64.25, Math.toRadians(-90)))
-                        .strafeTo(new Vector2d(48,28 ))
-                        .strafeTo(new Vector2d(48,38 ))
-                        .splineTo(new Vector2d(55,55), Math.toRadians(45))
-                        .waitSeconds(2)
-                        .strafeTo(new Vector2d(55,50))
-                        .turn(Math.toRadians(45))
-                        .splineTo(new Vector2d(58,28 ), Math.toRadians(-90))
-                        .waitSeconds(2)
-                        .strafeTo(new Vector2d(58,38))
-                        .splineTo(new Vector2d(55,55), Math.toRadians(45))
-                        .waitSeconds(2)
-                        .strafeTo(new Vector2d(45,45))
-                        .splineTo(new Vector2d(60,28), Math.toRadians(0))
-                        .waitSeconds(2)
-                        .strafeTo(new Vector2d(50,30))
-                        .splineTo(new Vector2d(55,55), Math.toRadians(45))
-                        .waitSeconds(2)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-24, 64.25, Math.toRadians(-90)))
+                        .strafeTo(new Vector2d(-8,34))
+                        .waitSeconds(3)
+                        .strafeTo(new Vector2d(-48,40))
+                        .lineToSplineHeading(new Pose2d(-48,60, Math.toRadians(90)))
+
                         .build());
 
 
