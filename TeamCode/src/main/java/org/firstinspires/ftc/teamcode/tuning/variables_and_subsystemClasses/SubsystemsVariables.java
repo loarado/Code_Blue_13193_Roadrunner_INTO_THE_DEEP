fd7340144ed588facdba1c7dd2227533a5ab714a
loarado <brauliokoalas@gmail.com>
2024-11-02 18:03:25 -0500

    Added more packages (folders), moved programs around, updated subsystems classes, and added more programs to test the subsystems.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/variables_and_subsystemClasses/SubsystemsVariables.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/variables_and_subsystemClasses/SubsystemsVariables.java
@@ -12,38 +12,38 @@ public class SubsystemsVariables {
     subsystems so if we need to change any we can do it easily without
     causing any problems. You welcome :) */
 
-    public final double OuttakeWristPos = 0.38;
+    public final double OuttakeWristPos = 0.58;
 
-    public final double IntakeWristPos = 0.58;
+    public final double IntakeWristPos = 0.38;
 
     public final double MiddleWristPos = 0.5;
 
-    public final double FrontIntakeWristPos = 0.64;
+    public final double FrontIntakeWristPos = 0.55;
 
 
-    public final double OuttakeElbowPos = 0.23;
+    public final double OuttakeElbowPos = 0.45;
 
-    public final double IntakeElbowPos = 0.55;
+    public final double IntakeElbowPos = 0.13;
 
-    public final double PrepIntakeElbowPos = 0.3;
+    public final double PrepIntakeElbowPos = 0.2;
 
-    public final double MiddleElbowPos = 0.5;
+    public final double MiddleElbowPos = 0.4;
 
-    public final double FrontIntakeElbowPos = 0.1;
+    public final double FrontIntakeElbowPos = 0.05;
 
-    public final double rightHandIn = -1;
-    public final double rightHandOut = 1;
+    public final double rightHandIn = 1;
+    public final double rightHandOut = -1;
     public final double rightHandStop = 0;
 
-    public final double leftHandIn = 1;
-    public final double leftHandOut = -1;
+    public final double leftHandIn = -1;
+    public final double leftHandOut = 1;
     public final double leftHandStop = 0;
 
-    public final int hSlidePhysicalMax = 800;
+    public final int hSlidePhysicalMax = 665;
     public final double hSlideSpeed = 0.5;
     public final double hSlideVelocity = 1300;
 
-    public final int vSlidePhysicalMax = 3800;
+    public final int vSlidePhysicalMax = 3700;
     public final int vSlideHighBasket = 3000;
     public final int vSlideLowBasket = 2000;
 
@@ -56,8 +56,8 @@ public class SubsystemsVariables {
     public final double vSlideVelocity = 1300;
 
     public final double robotLength = 15.5;
-    public final double backExtension = 6;
-    public final double slidesLength = 29.25;
+    public final double backExtension = 5;
+    public final double slidesLength = 22.5;
 
     public final int hSlideRuleMax = (int) (hSlidePhysicalMax*((slidesLength-((slidesLength + robotLength + backExtension) - (42)))/slidesLength));
 
