a7e0e56809eaf0dfe0229e1eb3cc049669f69ea3
loarado <brauliokoalas@gmail.com>
2024-11-10 22:18:35 -0600

    prep for LM2 auto testing. doing it the 'right way' this time

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LM1/MeepMeepTestingAlex.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LM1/MeepMeepTestingAlex.java
@@ -1,4 +1,4 @@
-package com.example.meepmeeptesting;
+package com.example.meepmeeptesting.LM1;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
@@ -7,40 +7,18 @@ import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTestingBasket {
+public class MeepMeepTestingAlex {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(36, 64.25, Math.toRadians(180)))
-                        .setReversed(true)
-                        .strafeTo(new Vector2d(40, 60))
-                        .splineTo(new Vector2d(54, 54), Math.toRadians(0))
-                        .waitSeconds(1)
-                        .setReversed(false)
-                        .strafeTo(new Vector2d(48, 48))
-                        .turn(Math.toRadians(45))
-                        .waitSeconds(1)
-                        .setReversed(true)
-                        .splineTo(new Vector2d(54, 54), Math.toRadians(45))
-                        .waitSeconds(1)
-                        .strafeTo(new Vector2d(54, 48))
-                        .turn(Math.toRadians(45))
-                        .strafeTo(new Vector2d(58.5, 48))
-                        .waitSeconds(1)
-                        .strafeTo(new Vector2d(57, 48))
-                        .splineTo(new Vector2d(54, 54), Math.toRadians(135))
-                        .waitSeconds(1)
-                        .setReversed(false)
-                        .splineTo(new Vector2d(40,12), Math.toRadians(180))
-                        .lineToLinearHeading(new Pose2d(new Vector2d(24,12), Math.toRadians(0)))
-                        .build());
-//test
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(36, 64.25, Math.toRadians(-90)))
 
-        /*
-        .waitSeconds(2)
+
+                        .strafeTo(new Vector2d(48, 40))
+                        .waitSeconds(2)
                         .turn(Math.toRadians(-45))
                         .strafeTo(new Vector2d(54,54))
                         .waitSeconds(2)
@@ -62,7 +40,8 @@ public class MeepMeepTestingBasket {
                         .splineTo(new Vector2d(40,12), Math.toRadians(180))
                         .strafeTo(new Vector2d(24,12))
                         .waitSeconds(2)
-         */
+                        .build());
+//test
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
                 .setDarkMode(true)
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LM1/MeepMeepTestingNicholas.java
@@ -0,0 +1,31 @@
+package com.example.meepmeeptesting.LM1;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTestingNicholas {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-24, 64.25, Math.toRadians(-90)))
+                        .strafeTo(new Vector2d(-8,34))
+                        .waitSeconds(3)
+                        .strafeTo(new Vector2d(-48,40))
+
+                        .build());
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
