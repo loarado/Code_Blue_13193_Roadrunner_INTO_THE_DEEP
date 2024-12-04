ee0c3dc7126d889733ac46363e2ee68f3fe23d4f
loarado <brauliokoalas@gmail.com>
2024-12-04 15:56:03 -0600

    changed lm2 auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LM2/ObservatoryLM2.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/LM2/ObservatoryLM2.java
@@ -16,13 +16,20 @@ public class ObservatoryLM2 {
                 .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-24, 64.25, Math.toRadians(90)))
                         .lineToSplineHeading(new Pose2d(0,36,Math.toRadians(90)))
+                        /* code for placing specimen on chamber*/
                         .splineToLinearHeading(new Pose2d(-33,48, Math.toRadians(270)),Math.toRadians(270))
-                        .strafeTo(new Vector2d(-36,12))
+                        .splineTo(new Vector2d(-36,12),Math.toRadians(270))
                         .splineToLinearHeading(new Pose2d(-46,12,Math.toRadians(270)),Math.toRadians(90))
                         .strafeTo(new Vector2d(-46,53))
                         .strafeTo(new Vector2d(-46,18))
                         .splineToLinearHeading(new Pose2d(-55,12,Math.toRadians(270)),Math.toRadians(90))
                         .strafeTo(new Vector2d(-55,53))
+                        .strafeTo(new Vector2d(-55,18))
+                        .splineToLinearHeading(new Pose2d(-61,12,Math.toRadians(270)),Math.toRadians(90))
+                        .strafeTo(new Vector2d(-61,53))
+                        .strafeTo(new Vector2d(-61,40))
+                        /*delay to allow human player to get samples out of observation zone before parking*/
+                        .strafeTo(new Vector2d(-61,63))
                         .build());
 
 
